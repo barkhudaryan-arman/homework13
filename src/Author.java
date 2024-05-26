893773589a74fad2bb492f0e3aa2d6ff02e9caad
barkhudaryan-arman <armanik147@gmail.com>
2024-05-26 22:43:31 +0300

    Проверьте ДЗ!

---
+++ src/Author.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+public class Author {
+    private final String name;
+    private final String lastName;
+
+    public Author(String name, String lastname) {
+        this.name = name;
+        this.lastName = lastname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
+    }
+}
